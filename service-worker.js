79d480f6a235799e29d03cd4dc8b5bebe49dfcdb
Felipe Moreira <engprocesso2@barbi.local>
2026-06-08 15:01:36 -0300

    melhorias_temp

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,85 @@
+const CACHE_NAME = "apontamento-isobarbi-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./logo.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
 
-  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+
+  self.skipWaiting();
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
 
 });
 
 self.addEventListener("fetch", event => {
+
+  if(event.request.method !== "GET"){
+    return;
+  }
+
+  if(
+    event.request.url.includes(
+      "script.google.com"
+    )
+  ){
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+
+      if(cached){
+        return cached;
+      }
+
+      return fetch(event.request).then(response => {
+
+        if(
+          !response ||
+          response.status !== 200 ||
+          response.type !== "basic"
+        ){
+          return response;
+        }
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+
+        return response;
+
+      });
+
+    })
+  );
 
 });
